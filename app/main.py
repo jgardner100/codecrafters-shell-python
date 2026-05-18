5f6a998d5921859067320b52cf0a8e92d094a8a9
John Gardner <jgardner100@hotmail.com>
2026-05-18 10:05:40 +1000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 import sys
 
 
+import sys
+
+
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    user_input = sys.stdin.readline()
-    if not user_input:
-        return
+        try:
+            command = input()
+        except EOFError:
+            break
 
-    command = user_input.strip()
-    if command:
+        command = command.strip()
+        if not command:
+            continue
+
         command_name = command.split()[0]
         print(f"{command_name}: command not found")
 
